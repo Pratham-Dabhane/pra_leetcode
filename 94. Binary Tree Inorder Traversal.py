0f2bb5f4393900caa1631ccba96d8c3465c07d01
pratham dabhane <pratham.dabhane.2503@gmail.com>
2025-01-11 23:50:19 +0530

    LeetCode Solutions

---
+++ 94. Binary Tree Inorder Traversal.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution(object):
+
+
+# Recursive Method 
+
+    def inorderTraversal(self, root):
+        res = []
+        stack = []
+        cur = root
+
+        # loop running till cur or stack is not Null
+        while cur or stack:
+
+            # loop until cur is not Null
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            res.append(cur)
+            cur = cur.right
+
+
+# Iterative Method
+            # Recursively calls the InOrder function for traversal
+            def inorder(root):
+                inorder(root.left)
+                res.append(root.val) 
+                inorder(root.right)
+            inorder(root)
+            return res
+        
